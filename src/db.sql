167e17ab06ff655918a93f7cc8938984dcf745eb
gsych <gennadiy.sych@gmail.com>
2012-12-14 00:17:17 +0400

    Finally made static resources available. Added initial db schema dump.

---
+++ src/db.sql
@@ -0,0 +1,79 @@
+/*
+SQLyog Community v10.5 
+MySQL - 5.5.28 : Database - appswanted
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`appswanted` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `appswanted`;
+
+/*Table structure for table `comments` */
+
+DROP TABLE IF EXISTS `comments`;
+
+CREATE TABLE `comments` (
+  `comment_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `post_id` bigint(20) unsigned NOT NULL,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `comment` text NOT NULL,
+  `score` int(11) DEFAULT NULL,
+  `ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`comment_id`),
+  KEY `FK_comments_users` (`user_id`),
+  KEY `FK_comments_posts` (`post_id`),
+  CONSTRAINT `FK_comments_posts` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_comments_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `comments` */
+
+/*Table structure for table `posts` */
+
+DROP TABLE IF EXISTS `posts`;
+
+CREATE TABLE `posts` (
+  `post_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `parent_id` bigint(20) unsigned DEFAULT NULL,
+  `content` text NOT NULL,
+  `tags` varchar(150) DEFAULT NULL,
+  `score` int(11) DEFAULT NULL,
+  `ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`post_id`),
+  KEY `FK_app_ideas_users` (`user_id`),
+  CONSTRAINT `FK_app_ideas_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `posts` */
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `user_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(256) NOT NULL,
+  `activation_code` varchar(64) DEFAULT NULL,
+  `activated` tinyint(1) NOT NULL DEFAULT '0',
+  `display_name` varchar(30) NOT NULL,
+  `register_date` datetime NOT NULL,
+  `status` enum('active','blocked','deleted') NOT NULL DEFAULT 'active',
+  `ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `users` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
--- src/main/java/com/appswanted/ApplicationServlet.java
+++ src/main/java/com/appswanted/ApplicationServlet.java
@@ -1,9 +1,13 @@
 package com.appswanted;
 
 import com.appswanted.controllers.IndexController;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.webapp.WebAppContext;
 import org.featherj.EntryServlet;
 import org.featherj.routes.Route;
 import org.featherj.routes.Router;
@@ -14,18 +18,38 @@ public class ApplicationServlet extends EntryServlet {
     @Override
     protected Route[] routes() throws UrlParseException {
         return new Route[] {
-            Router.route("/", IndexController.Index),
-            Router.resourceRoute("/css/*")
+            Router.route("/", IndexController.Index)
         };
     }
 
     public static void main(String[] args) throws Exception{
         //Server server = new Server(Integer.valueOf(System.getenv("PORT")));
         Server server = new Server(Integer.valueOf(5000));
+
+        WebAppContext css = new WebAppContext();
+        css.setServer(server);
+        css.setContextPath("/css");
+        css.setWar("target/classes/css");
+
+        WebAppContext js = new WebAppContext();
+        js.setServer(server);
+        js.setContextPath("/js");
+        js.setWar("target/classes/js");
+
+        WebAppContext img = new WebAppContext();
+        img.setServer(server);
+        img.setContextPath("/img");
+        img.setWar("target/classes/img");
+
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
-        server.setHandler(context);
-        context.addServlet(new ServletHolder(new ApplicationServlet()),"/*");
+        context.addServlet(new ServletHolder(new ApplicationServlet()), "/*");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[] { css, js, img, context });
+
+        server.setHandler(handlers);
+
         server.start();
         server.join();
     }
